6ff6b9cc8d7f1654142fc8927b29daa46467d6fd
Lali <lalita.su.61@ubu.ac.th>
2021-07-16 17:38:06 +0700

    edit capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'blog-app',
-  webDir: 'build',
+  webDir: 'www',
   bundledWebRuntime: false
 };
 
