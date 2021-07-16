a82db5c14150a23409f48f4436c19dad08d787a8
Lali <lalita.su.61@ubu.ac.th>
2021-07-16 17:04:20 +0700

    test2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,7 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'blog-app',
   webDir: 'www',
-  // bundledWebRuntime: false
+  bundledWebRuntime: false
 };
 
 export default config;
